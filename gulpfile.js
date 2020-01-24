a4124cd54798522d2e74e2394c4d87a47f20c703
Sergey Orlov <skillzorwispa@gmail.com>
2020-01-24 17:40:33 +0700

    add gulp tree

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+global.$ = {
+    gulp: require('gulp'),
+    gulpPlugins: require('gulp-load-plugins')(),
+    browserSync: require('browser-sync').create(),
+    del: require('del'),
+    imageJpegCompress: require('imagemin-jpeg-recompress'),
+    pngquant: require('imagemin-pngquant')
+};
