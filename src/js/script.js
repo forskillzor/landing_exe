c95fe64dcf89cfeee70df0165b29ca1fbbeea8c6
Sergey Orlov <skillzorwispa@gmail.com>
2020-01-28 17:57:00 +0700

    add progress bar script

--- src/js/script.js
+++ src/js/script.js
@@ -8,4 +8,16 @@ $(document).ready(function(){
         $(this).parents('.works__info').removeClass('active');
         $(this).removeClass('active');
     });
+
+    function moveProgressBar(node, nodeLine, tooltip, animationLength = 1500){
+        const progressElement = $(node);
+        progressElement.each(function (value, item) {
+            $(item).find(nodeLine).animate({
+                width: item.dataset.progress+'%'
+            }, animationLength);
+            $(item).find(tooltip).show(animationLength);
+        });
+    }
+
+    moveProgressBar('.progress__bar', '.progress__line', '.progress__tooltip');
 });
