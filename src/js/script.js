863f2f284446613ecaaf02f6c64b513583d0d8ce
Sergey Orlov <skillzorwispa@gmail.com>
2020-01-30 08:37:10 +0700

    section.clients carousel bagan

--- src/js/script.js
+++ src/js/script.js
@@ -27,5 +27,25 @@ $(document).ready(function(){
         }
     });
 
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:false,
+        dots: false,
+        navText:[],
+        itme:5,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    })
+
 });
 
